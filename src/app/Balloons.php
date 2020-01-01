afbbf08cae229ac2e2cc5a4762ee6525c4301fe5
yukimi-summer <yusuke.1226.d@gmail.com>
2020-01-01 17:32:30 +0900

    make relation messages and balloons.

--- src/app/Balloons.php
+++ src/app/Balloons.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Balloons extends Model
 {
-    //
+    public function message()
+    {
+        return $this->belongsTo('App\Message');
+    }
 }
--- src/app/Messages.php
+++ src/app/Messages.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Messages extends Model
 {
-    //
+    public function balloons()
+    {
+        return $this->hasMany('App\Balloons');
+    }
 }
